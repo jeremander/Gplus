ae7eca9d8be32e96551ae0ede53a2d812d6e2f48
Jeremy Silver <jeremander@Jeremys-MacBook-Pro.local>
2015-11-23 11:17:37 -0500

    Added degree & component size plots.

--- gplus.py
+++ gplus.py
@@ -11,7 +11,7 @@ def time_format(seconds):
     if (seconds >= 3600):
         s += "%dh," % (seconds // 3600)
     if (seconds >= 60):
-        s += "%2dm," % (seconds // 60)
+        s += "%dm," % (seconds // 60)
     s += "%.3fs" % (seconds % 60)
     return s
 
@@ -25,8 +25,8 @@ def timeit(func):
         return result
     return timed
 
-def autoreadwrite(obj_name, extension):
-    """Decorator for methods that construct a member object. Initially tries to load the member object from a file in the object's 'folder' member, whose filename is the constructed member object with either a .csv or .pickle extension, depending on the format."""
+def autoreadwrite(obj_names, extensions):
+    """Decorator for methods that construct member objects. Initially tries to load each member object from a file in the object's 'folder' member, whose filename is the constructed member object with either a .csv or .pickle extension, depending on the format."""
     def autoreadwrite_decorator(func):
         def autoreadwrite_wrapped_function(*args, **kwargs):
             self = args[0]
@@ -37,58 +37,60 @@ def autoreadwrite(obj_name, extension):
                 save = kwargs['save']
             else:
                 save = False
-            filename = self.folder + '/' + obj_name + '.' + extension
-            if (not hasattr(self, obj_name)):
-                did_load = False
-                try:
-                    print("Loading data from '%s'..." % filename)
-                    start_time = time.time()
-                    if (extension == 'csv'):
-                        self.__dict__[obj_name] = timeit(pd.read_csv)(filename, index = False)
-                        did_load = True
-                    elif (extension == 'pickle'):
-                        self.__dict__[obj_name] = timeit(pickle.load)(open(filename, 'rb'))
-                        did_load = True
-                    if hasattr(self, obj_name):
-                        print("Successfully loaded data.")
-                except:
-                    pass
-                if (not did_load):
-                    print("Could not load object from file.")
-                    timeit(func)(*args, **kwargs)
-            if save:
-                did_save = False
-                try:
-                    print("Saving data to '%s'..." % filename)
-                    if (extension == 'csv'):
-                        timeit(pd.DataFrame.to_csv)(self.__dict__[obj_name], filename, index = False)
-                        did_save = True
-                    elif (extension == 'pickle'):
-                        timeit(pickle.dump)(self.__dict__[obj_name], open(filename, 'wb'))
-                        did_save = True
-                except:
-                    pass
-                if did_save:
-                    print("Successfully saved data.")
-                else:
-                    print("Failed to save object to file.")
-            return self.__dict__[obj_name]
+            for obj_name, extension in zip(obj_names, extensions):
+                filename = self.folder + '/' + obj_name + '.' + extension
+                if (not hasattr(self, obj_name)):
+                    did_load = False
+                    try:
+                        print("\nLoading %s from '%s'..." % (obj_name, filename))
+                        start_time = time.time()
+                        if (extension == 'csv'):
+                            self.__dict__[obj_name] = timeit(pd.read_csv)(filename, index = False)
+                            did_load = True
+                        elif (extension == 'pickle'):
+                            self.__dict__[obj_name] = timeit(pickle.load)(open(filename, 'rb'))
+                            did_load = True
+                        if hasattr(self, obj_name):
+                            print("Successfully loaded %s." % obj_name)
+                    except:
+                        pass
+                    if (not did_load):
+                        print("Could not load %s from file." % obj_name)
+                        timeit(func)(*args, **kwargs)
+                if save:
+                    did_save = False
+                    try:
+                        print("Saving %s to '%s'..." % (obj_name, filename))
+                        if (extension == 'csv'):
+                            timeit(pd.DataFrame.to_csv)(self.__dict__[obj_name], filename, index = False)
+                            did_save = True
+                        elif (extension == 'pickle'):
+                            timeit(pickle.dump)(self.__dict__[obj_name], open(filename, 'wb'))
+                            did_save = True
+                    except:
+                        pass
+                    if did_save:
+                        print("Successfully saved %s." % obj_name)
+                    else:
+                        print("Failed to save %s to file." % obj_name)
+            return self.__dict__[obj_names[0]]  # return only the first object (others are stored)
         return autoreadwrite_wrapped_function
     return autoreadwrite_decorator
 
 
 class Gplus(ig.Graph):
     """Graph of Google+ data."""
-    @autoreadwrite('degree_dict', 'pickle')
+    @autoreadwrite(['degree_dict', 'degree_power_law'], ['pickle', 'pickle'])
     def degrees(self, save = False):
         """Returns dictionary of node degrees."""
         self.degree_dict = dict((v.index, v.degree()) for v in self.vs)
-    @autoreadwrite('comp_sizes', 'csv')
+        self.degree_power_law = ig.power_law_fit(list(self.degree_dict.values()))
+    @autoreadwrite(['comp_sizes'], ['csv'])
     def component_sizes(self, save = False):
         """Returns connected component sizes."""
         components = self.components()
         self.comp_sizes = pd.DataFrame(np.array([len(components.subgraph(i)) for i in range(len(components))]), columns = ['componentSize'])
-    @autoreadwrite('fastgreedy_comms', 'pickle')
+    @autoreadwrite(['fastgreedy_comms'], ['pickle'])
     def fastgreedy_communities(self, save = False):
         """Returns communities obtained by iGraph's fastgreedy algorithm (approximately linear time)."""
         self.fastgreedy_comms = self.community_fastgreedy()
--- gplus_plots.py
+++ gplus_plots.py
@@ -1,20 +1,32 @@
 import pickle
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 from ggplot import *
 
 folder = "gplus0"
 #folder = "gplus0_sample1M"
 
-# degree histogram
+# degree histogram and power law fit
 degree_dict = pickle.load(open(folder + '/data/degree_dict.pickle', 'rb'))
+power_law = pickle.load(open(folder + '/data/degree_power_law.pickle', 'rb'))
+degrees = list(degree_dict.values())
+degree_counts = defaultdict(int)
+for deg in degrees:
+    degree_counts[deg] += 1
+del(degree_counts[0])
+log10C = np.mean(np.array([power_law.alpha * np.log10(deg) + np.log10(count) for (deg, count) in degree_counts.items()]))
+power_law_df = pd.DataFrame(columns = ['degree', 'fittedCount'])
+power_law_df['degree'] = 10 ** (np.arange(0, np.log10(max(degrees) * 1.2), 0.1))
+power_law_df['fittedCount'] = power_law_df['degree'].map(lambda x : 10 ** (log10C - power_law.alpha * np.log10(x)))
+power_law_df = power_law_df[power_law_df['fittedCount'] >= 0.1]
 degree_df = pd.DataFrame(list(degree_dict.values()), columns = ['degree'])
-degree_hist = ggplot(aes(x = 'degree'), data = degree_df) + geom_histogram(binwidth = 20., fill = 'royalblue', color = 'black') + scale_x_log10() + scale_y_log10() + ggtitle("Degree distribution") + xlab("node degree")
+degree_plot = ggplot(aes(x = 'degree'), data = degree_df) + geom_histogram(binwidth = 20., fill = 'royalblue', color = 'black') + scale_x_log10() + scale_y_log10() + ggtitle("Degree distribution\nPower law alpha = %.3f, log10(intercept) = %.3f" % (power_law.alpha, log10C)) + xlab("node degree") + ylab("")
+degree_plot += geom_line(aes(x = 'degree', y = 'fittedCount'), data = power_law_df, size = 3, color = 'red') + xlim(low = 0.5, high = max(degrees) * 1.1) + ylim(low = 0.5, high = max(power_law_df['fittedCount'] * 1.1))
 
-# component size histogram
+# component size rank plot
 comp_size_df = pd.read_csv(folder + '/data/comp_sizes.csv').sort_values(by = 'componentSize', ascending = False)
 comp_size_plot = ggplot(aes(x = list(range(len(comp_size_df))), y = comp_size_df['componentSize']), data = comp_size_df) + geom_point(size = 20, color = 'maroon') + scale_y_log10() + ggtitle("Component sizes") + scale_x_continuous(breaks = list(np.arange(0, 1200, 100))) + xlim(low = -25, high = 1175) + xlab("rank") + ylim(low = 0.5, high = 10**7) + ylab("comp size")
 
-
-ggsave(folder + '/plots/degree_hist', degree_hist)
+ggsave(folder + '/plots/degree_plot', degree_plot)
 ggsave(folder + '/plots/component_size_plot', comp_size_plot)
